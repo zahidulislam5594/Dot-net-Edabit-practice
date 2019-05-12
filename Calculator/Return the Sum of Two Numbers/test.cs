824428b98cf0528372372ac71dbdf7c4b9318979
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:29:05 +0600

    Create test.cs

---
+++ Calculator/Return the Sum of Two Numbers/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
