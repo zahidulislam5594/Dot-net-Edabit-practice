30e732ad577e569ad341b0163777f6bf7a87785d
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:40:25 +0600

    Create test17.cs

---
+++ Array/Return the Last Element in an Array/test17.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    class test17
+    {
+        public static object GetLastItem(object[] arr)
+        {
+            return arr[arr.Length - 1];
+        }
+    }
+}
