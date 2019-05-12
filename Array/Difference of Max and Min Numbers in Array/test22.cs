18cc429c762e91bd0ccaffd86651882a0ef514ca
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 00:04:02 +0600

    Create test22.cs

---
+++ Array/Difference of Max and Min Numbers in Array/test22.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test22
+    {
+        public static int differenceMaxMin(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+                else if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            return max - min;
+        }
+    }
+}
