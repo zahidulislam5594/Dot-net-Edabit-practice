7e64a914f37acc3265f2e4b536e764726832abb4
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:55:19 +0600

    Create test10.cs

---
+++ String/Concatenate First and Last Name into One String/test10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test10
+    {
+        public static string ConcatName(string firstName, string lastName)
+        {
+            return ($"{lastName}, {firstName}");
+        }
+    }
+}
