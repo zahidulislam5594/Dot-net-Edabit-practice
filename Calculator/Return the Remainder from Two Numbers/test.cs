e1cfaf49097746fabe622d7726c402494fd5449d
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:29:11 +0600

    Create test.cs

---
+++ Calculator/Return the Remainder from Two Numbers/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test4
+    {
+        public static int Remainder(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
