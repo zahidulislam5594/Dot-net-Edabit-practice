06b5c92753efd9b888859572e8434ea8eeb568c1
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:47:11 +0600

    Create test6.cs

---
+++ Calculator/The Farm Problem/test6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test6
+    {
+        public static int animals(int chickens, int cows, int pigs)
+        {
+            return chickens * 2 + cows * 4 + pigs * 4;
+        }
+    }
+}
