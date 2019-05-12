0869941f66847538f968ffebe22b154489c04eab
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:21:17 +0600

    Create test16.cs

---
+++ String/Check String for Spaces/test16.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test16
+    {
+        public static bool HasSpaces(string str)
+        {
+            bool b = str.Contains(" ");
+            if (b == true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
