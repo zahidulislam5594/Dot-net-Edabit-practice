23b2e42d39a0c8563b8335fde2825f40f54b7341
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:29:15 +0600

    Create test.cs

---
+++ Calculator/Return the Next Number from the Integer Passed/test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test3
+    {
+        public static int Addition(int num)
+        {
+            int sum = 1;
+            sum = sum + num;
+            return sum;
+        }
+    }
+}
