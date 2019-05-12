a30b46c98248f188a2d452d7c3afc96ab07546a0
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:09:03 +0600

    Create test11.cs

---
+++ Calculator/Is it Time for Milk and Cookies/test11.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test11
+    {
+        public static bool TimeForMilkAndCookies(int year, int month, int day)
+        {
+            return (day == 24 && month == 12);
+        }
+    }
+}
