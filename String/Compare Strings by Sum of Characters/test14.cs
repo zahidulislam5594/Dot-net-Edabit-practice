1f76744516d4711c94b782904edce0712938c475
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:14:54 +0600

    Create test14.cs

---
+++ String/Compare Strings by Sum of Characters/test14.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test14
+    {
+        public static bool Comp(string str1, string str2)
+        {
+            if (str1.Length == str2.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
