b184b68c7175a1c798f0f1f99d15a6504a6d1a1a
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:40:21 +0600

    Create test18.cs

---
+++ String/Is the Word Singular or Plural/test18.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test18
+    {
+        public static bool IsPlural(string word)
+        {
+            bool b = word.EndsWith("s");
+            if (b == true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
