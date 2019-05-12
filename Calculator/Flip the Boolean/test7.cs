47403590b1134ffd174a22b2305536b4829b878a
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:47:16 +0600

    Create test7.cs

---
+++ Calculator/Flip the Boolean/test7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test7
+    {
+        public static bool Reverse(bool boolean)
+        {
+            if (boolean == true)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
