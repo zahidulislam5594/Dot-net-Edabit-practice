764ca82a3a2cc26cd5c8299f9824d10f2ea419ab
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:29:22 +0600

    Create test.cs

---
+++ Calculator/Area of a Triangle/test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test1
+    {
+        public static int triArea(int b, int h)
+        {
+            return (b * h) / 2;
+        }
+    }
+}
