278b8c6eeede4d798920979a02f3974dbae70733
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:09:00 +0600

    Create test12.cs

---
+++ Calculator/Multiple of 100/test12.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test12
+    {
+        public static bool Divisible(int number)
+        {
+            if (number % 100 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
