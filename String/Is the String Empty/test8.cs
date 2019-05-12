4995632c331fd52ae285c602de82917a03678af0
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:47:06 +0600

    Create test8.cs

---
+++ String/Is the String Empty/test8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test8
+    {
+        public static bool isEmpty(string str)
+        {
+            if (str == "")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
