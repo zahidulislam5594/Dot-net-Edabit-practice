00a8aa0fa291c9c930a94228c8d5d4e8b41b9e6f
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 03:41:09 +0600

    Create test24.cs

---
+++ Calculator/Missing Third Angle/test24.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test24
+    {
+        public static string MissingAngle(int angle1, int angle2)
+        {
+            int angle = 180 - (angle1 + angle2);
+
+            if (angle < 90) return "acute";
+            else if (angle == 90) return "right";
+            else if (angle > 90 && angle < 180) return "obtuse";
+            else return "invalid";
+
+        }
+    }
+}
