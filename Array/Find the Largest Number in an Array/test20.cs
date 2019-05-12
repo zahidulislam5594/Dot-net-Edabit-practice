1652bcef8f10ae0dad7e19130b27e27adc790c82
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 00:03:59 +0600

    Create test20.cs

---
+++ Array/Find the Largest Number in an Array/test20.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    class test20
+    {
+        public static int FindLargestNum(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+    }
+}
