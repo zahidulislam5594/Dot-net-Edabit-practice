1f78e0d27a8a12ddf350522f1d3179ce245b35da
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:21:21 +0600

    Create test15.cs

---
+++ Calculator/Is the Number Even or Odd/test15.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test15
+    {
+        public static string IsEvenOrOdd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return "even";
+            }
+            return "odd";
+        }
+    }
+}
