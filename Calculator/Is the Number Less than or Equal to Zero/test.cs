61ecedb104228e9b4d880fa4617da5cc2d136d54
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:29:19 +0600

    Create test.cs

---
+++ Calculator/Is the Number Less than or Equal to Zero/test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test5
+    {
+        public static bool LessThanOrEqualToZero(double a)
+        {
+            if (a <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
