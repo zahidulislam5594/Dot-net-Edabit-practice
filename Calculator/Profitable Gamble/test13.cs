ebff645c8602cd2c91e8d213e099e7eec6e4190b
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 23:14:59 +0600

    Create test13.cs

---
+++ Calculator/Profitable Gamble/test13.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test13
+    {
+        public static bool ProfitableGamble(double prob, int prize, double pay)
+        {
+            if (prob * prize > pay)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
