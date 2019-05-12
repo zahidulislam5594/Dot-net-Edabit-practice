98d101246a3ce9c452f200c288280c63df9572bc
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-12 22:55:14 +0600

    Create test9.cs

---
+++ Calculator/Check if an Integer is Divisible By Five/test9.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test9
+    {
+        public static bool divisibleByFive(int n)
+        {
+            if (n % 5 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
