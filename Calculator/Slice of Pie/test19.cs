be3fb3acc4dcfb8c9043f345b6113a1351af70f6
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 00:03:48 +0600

    Create test19.cs

---
+++ Calculator/Slice of Pie/test19.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test19
+    {
+        public static bool EqualSlices(int total, int people, int each)
+        {
+            if (people * each <= total)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
