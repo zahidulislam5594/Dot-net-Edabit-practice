0664450ef19691ca1ea080cd98f4f1d105af9f7b
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:29:12 +0600

    Create test29.cs

---
+++ Calculator/Find the Total Number of Digits the Given Number Has/test29.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test29
+    {
+        public static int FindDigitAmount(int num)
+        {
+            return num.ToString().Length;
+        }
+    }
+}
