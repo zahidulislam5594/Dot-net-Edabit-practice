b3867fcf258fc8b68285e5b3412ed040000e089c
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:29:16 +0600

    Create test27.cs

---
+++ String/Count Syllables/test27.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test27
+    {
+        public static int NumberSyllables(string word)
+        {
+            string[] word1 = word.Split('-');
+            return word1.Length;
+        }
+    }
+}
