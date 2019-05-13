408d7473ef613f757a1dba9f44ea273ce42711eb
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:37:41 +0600

    Create test32.cs

---
+++ Array/Absolute Sum/test32.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test32
+    {
+        public static int GetAbsSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + System.Math.Abs(arr[i]);
+            }
+            return sum;
+        }
+    }
+}
