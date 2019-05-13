d46dd73f599e861f5500c8949ab86fcd33aa775d
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:29:19 +0600

    Create test26.cs

---
+++ String/Get Word Count/test26.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test26
+    {
+        public static int CountWords(string str)
+        {
+            string[] str1 = str.Split();
+            return str1.Length;
+        }
+    }
+}
