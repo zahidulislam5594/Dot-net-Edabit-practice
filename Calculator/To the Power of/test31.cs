da0e160b80ae213ab3dd3bf4645a240268a33999
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:37:36 +0600

    Create test31.cs

---
+++ Calculator/To the Power of/test31.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test31
+    {
+        public static long CalculateExponent(long number, long exponent)
+        {
+            return (long)System.Math.Pow(number, exponent);
+        }
+    }
+}
