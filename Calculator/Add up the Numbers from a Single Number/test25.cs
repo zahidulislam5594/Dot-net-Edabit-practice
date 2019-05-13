7e2865b8fab8e22f2e4b2677fc402fd45e180100
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:29:04 +0600

    Create test25.cs

---
+++ Calculator/Add up the Numbers from a Single Number/test25.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test25
+    {
+        public static int AddUp(int num)
+        {
+            int sum = 0;
+            for (int i = 0; i <= num; i++)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+    }
+}
