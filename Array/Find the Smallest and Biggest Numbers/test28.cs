b8f31b4f3d9371ad44d56060af82bc8adffff6ac
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:28:59 +0600

    Create test28.cs

---
+++ Array/Find the Smallest and Biggest Numbers/test28.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test28
+    {
+        public static double[] FindMinMax(double[] values)
+        {
+            Console.WriteLine(values.Min());
+            Console.WriteLine(values.Max());
+            return new double[] { values.Min(), values.Max()};
+        }
+    }
+}
