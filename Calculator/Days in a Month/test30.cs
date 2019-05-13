58b7350aa2ea9a4297af8b5cb74f91e4853f1adc
zahidulislamniloy <39778420+zahidulislamniloy@users.noreply.github.com>
2019-05-13 12:29:08 +0600

    Create test30.cs

---
+++ Calculator/Days in a Month/test30.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_net_Edabit_practice
+{
+    public class test30
+    {
+        public static int Days(int month, int year)
+        {
+            return System.DateTime.DaysInMonth(year, month);
+
+        }
+    }
+}
